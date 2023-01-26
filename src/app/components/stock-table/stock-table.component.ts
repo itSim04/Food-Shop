57c4784bfc3b0ae1a7dc73d653b1eb06339fac67
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-01-26 17:03:21 +0200

    Implemented the table rows

--- src/app/components/stock-table/stock-table.component.ts
+++ src/app/components/stock-table/stock-table.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Product {
+
+  name: string,
+  price: number,
+  availability: boolean;
+
+}
+
 @Component({
   selector: 'app-stock-table',
   templateUrl: './stock-table.component.html',
   styleUrls: ['./stock-table.component.scss']
 })
 export class StockTableComponent {
+
+  products: Product[] = [
+
+    {
+      availability: true,
+      name: 'Apple',
+      price: 2
+    },
+    {
+      availability: false,
+      name: 'Banana',
+      price: 1
+    }
+
+  ];
 
 }
