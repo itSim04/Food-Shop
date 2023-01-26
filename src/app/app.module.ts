e45551c1817036ddef465e3bd3c34ef2304577f0
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2023-01-26 13:44:59 +0200

    Started designing the stock table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { StockTableModule } from './components/stock-table/stock-table.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StockTableComponent } from './components/stock-table/stock-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StockTableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StockTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/stock-table/stock-table.module.ts
+++ src/app/components/stock-table/stock-table.module.ts
@@ -2,14 +2,15 @@ import { StockTableComponent } from './stock-table.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     StockTableComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule
   ],
   exports: [
     StockTableComponent
